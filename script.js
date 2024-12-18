7197b71061a096a6f3b938bf87f9788c61d41731
MR.PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2024-12-18 13:47:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function  Time(){
+   var Time=new Date();
+   var a=Time.toLocaleTimeString();
+   document.getElementById("Timebtn").innerHTML=a;
+
+}
+setInterval(Time,100)
+// for home button only //
+
+function Scrollbtn(){
+
+ window.scrollTo(0, -1000);
+      
+
+}
+
+
+  function formCheck(){
+    a=document.getElementById("uname").value;
+    b=document.getElementById("uemail").value;
+    c=document.getElementById("umsg").value;
+    var Objtext={
+      Name :a,
+      Email:b,
+      message:c,
+    }
+    var Strtext=JSON.stringify(Objtext);
+    if(a!=" "&&b!=" "&&c!=" ")
+    {
+      localStorage.setItem(b,Strtext);
+      alert("Your message sucessfully sent");
+    }
+    else alert("All field is requi#A3CCE3 !");
+
+  }
+
+function openNav() {
+  document.getElementById("result").style.display="block";
+}
+
+function closeNav() {
+  document.getElementById("result").style.display="none";
+}
+  
